39593aa7cab81a9ae0940bdda4c597f837619a3b
Benjamin Pierce <bcpierce@seas.upenn.edu>
2026-06-16 09:57:11 -0400

    Finished versifying Basics.lean!

--- LF.lean
+++ LF.lean
@@ -2,11 +2,11 @@ import SFLMeta.Bnf
 import SFLMeta.Ignore
 import SFLMeta.Save
 
-import LF.BasicsVerso
+import LF.Basics
 
 import VersoManual
 
 open Verso Genre Manual
 
 #doc (Manual) "Logical Foundations" =>
-{include LF.BasicsVerso}
+{include LF.Basics}
